b95f9be26374365836ef03b17dc491fe43a14db5
Andrea Svegliado <andrea.svegliado@gmail.com>
2021-08-12 21:26:18 +0200

    ..

--- src/app/_models/CLS_Classe.ts
+++ src/app/_models/CLS_Classe.ts
@@ -3,4 +3,6 @@ export interface CLS_Classe {
   descrizioneBreve:     string;
   descrizione:          string;
   descrizione2:         string;
+  importo:              number;
+  importo2:             number;
 }
