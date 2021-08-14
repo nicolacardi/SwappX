1ee18b4fb9313a7f4ed0f9fbbbf16c3a5f564806
nicolacardi <nicola.cardi@gmail.com>
2021-08-14 18:12:52 +0200

    ...

--- src/app/_components/pagamenti/rette-list/rette-list.component.ts
+++ src/app/_components/pagamenti/rette-list/rette-list.component.ts
@@ -1,7 +1,8 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { Observable } from 'rxjs';
+import { from, Observable, of, zip } from 'rxjs';
+import { flatMap, groupBy, map, mergeMap, toArray } from 'rxjs/operators';
 import { PAG_Retta } from 'src/app/_models/PAG_Retta';
 import { LoadingService } from '../../utilities/loading/loading.service';
 import { RetteService } from '../rette.service';
@@ -44,16 +45,68 @@ export class RetteListComponent implements OnInit {
       
     obsRette$= this.svcRette.load();
 
-    const loadPagamenti$ =this._loadingService.showLoaderUntilCompleted(obsRette$);
+    //const loadRette$ =this._loadingService.showLoaderUntilCompleted(obsRette$);
 
-    loadPagamenti$.subscribe(val => 
+    // of ({id: 1, name: 'aze1'},
+    // {id: 2, name: 'sf2'},
+    // {id: 2, name: 'dg2'},
+    // {id: 1, name: 'erg1'},
+    // {id: 1, name: 'df1'},
+    // {id: 2, name: 'sfqfb2'},
+    // {id: 3, name: 'qfs1'},
+    // {id: 2, name: 'qsgqsfg2'})
+    // .pipe(
+
+    //   groupBy(p => p.id, p=>p.name),
+    //   mergeMap( (group$) => group$.reduce((x:any, y:any) => [...x, y], [group$.key])),  //ma sarebbe flatMap che è deprecated
+    //   //map(arr => ({'mese': (arr[0], 'values': arr.slice(1)}))
+    // ).subscribe(p=> console.log(p))
+
+
+    // const people = [
+    //   { name: 'Sue', age: 25 },
+    //   { name: 'Joe', age: 30 },
+    //   { name: 'Frank', age: 25 },
+    //   { name: 'Sarah', age: 35 }
+    // ];
+    // //emit each person
+    // const source = from(people);
+    // //group by age
+    // const example = source.pipe(
+    //   groupBy(person => person.age),
+    //   // return each item in group as array
+    //   mergeMap(group => group.pipe(toArray()))
+    // );
+
+    // const subscribe = example.subscribe(val => console.log("cicciopasticcio", val));
+
+    
+
+    obsRette$
+    .pipe(
+      mergeMap(res=>res),
+      groupBy(o => o.mese, q=>q.quotaConcordata),
+      // return each item in group as array
+      //mergeMap(group => group.pipe(toArray())),
+      mergeMap(group => zip(["" + group.key], group.pipe(toArray()))),
+      //mergeMap( (group$) => group$.reduce((x:any, y:any) => [...x, y], ["" + group$.key]))
+      map(arr => ({'mese': parseInt(arr[0]), 'quote': arr.slice(1)}))
+    )
+      .subscribe(val => 
       {
-        this.matDataSource.data = val;
+        console.log(val);
+        //this.matDataSource.data = val;
         //this.matDataSource.paginator = this.paginator;
         //this.matDataSource.sort = this.sort;
       }
     );
+
+
+
   }
+
+
+  
 
 
 
