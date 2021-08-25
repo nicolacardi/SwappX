aa83c6a1c3b9f7f5313152f8819213012f0f43a2
nicolacardi <nicola.cardi@gmail.com>
2021-08-26 00:19:36 +0200

    annoID

--- src/app/_components/pagamenti/pagamenti-list/pagamenti-list.component.ts
+++ src/app/_components/pagamenti/pagamenti-list/pagamenti-list.component.ts
@@ -1,5 +1,5 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -9,6 +9,9 @@ import { PAG_Pagamento } from 'src/app/_models/PAG_Pagamento';
 import { PagamentoEditComponent } from '../pagamento-edit/pagamento-edit.component';
 import { PagamentiService } from '../pagamenti.service';
 import { LoadingService } from '../../utilities/loading/loading.service';
+import { ASC_AnnoScolastico } from 'src/app/_models/ASC_AnnoScolastico';
+import { AnniScolasticiService } from 'src/app/_services/anni-scolastici.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-pagamenti-list',
@@ -18,6 +21,13 @@ import { LoadingService } from '../../utilities/loading/loading.service';
 
 export class PagamentiListComponent implements OnInit {
 
+  @Input() dove!:           string;
+  //@Input() alunnoID!:       number;
+
+  obsAnni$!: Observable<ASC_AnnoScolastico[]>;    //Serve per la combo anno scolastico
+  form:                     FormGroup;            //form fatto della sola combo anno scolastico
+
+  show: boolean = true;
   matDataSource = new MatTableDataSource<PAG_Pagamento>();
   displayedColumns: string[] =  [];
   displayedColumnsList: string[] = [
@@ -36,36 +46,76 @@ export class PagamentiListComponent implements OnInit {
                                   "alunno.nome",
                                   //"genitoreID",
                                   "note"];
+  displayedColumnsListRettaEdit: string[] = [
+                                   
+                                  "dtPagamento", 
+                                  "importo", 
+                                  "tipoPagamento.descrizione",
+                                  "causale.descrizione",
+                                  "retta.mese",
+                                     ];
 
   menuTopLeftPosition =  {x: '0', y: '0'} 
   matMenuTrigger: any;
 
   constructor(private svcPagamenti:     PagamentiService,
+              private svcAnni:          AnniScolasticiService,
               public _dialog:           MatDialog, 
               private _loadingService:  LoadingService,
-    ) {
-  
+              private fb:               FormBuilder, 
+    ) 
+  {
+    //form composto della sola combo Anno Scolastico: così si riesce tra le altre cose a settare un valore di default
+    this.form = this.fb.group({
+      annoScolastico:      [1],
+    });
   }
 
   ngOnInit(): void {
-    this.displayedColumns =  this.displayedColumnsList;
+
+    if (this.dove == 'retta-edit') {
+      this.show = false;
+      this.displayedColumns =  this.displayedColumnsListRettaEdit;
+    } else {
+      this.show = true;
+      this.displayedColumns =  this.displayedColumnsList;
+    }
     this.refresh();
   }
 
+  updateList() {
+    //TODO: bisogna ri-attivare refresh per caricare la lista dell'anno selezionato
+    this.refresh();
+  }
+
+
+
+
+
   refresh () {
+
+    this.obsAnni$= this.svcAnni.load();
+
     let obsPagamenti$: Observable<PAG_Pagamento[]>;
-      
-    obsPagamenti$= this.svcPagamenti.load();
 
-    const loadPagamenti$ =this._loadingService.showLoaderUntilCompleted(obsPagamenti$);
+    
+    console.log("annoID dalla combobox", this.form.controls['annoScolastico'].value);
+    //console.log("this.alunnoID", this.alunnoID);
+    
+    //TODO serve filtrare l'observable obsPagamenti in base al valore selezionato nella dropdwon anno scolastico
+    obsPagamenti$= this.svcPagamenti.loadByAnno(this.form.controls['annoScolastico'].value);
 
-    loadPagamenti$.subscribe(val => 
+    //const loadPagamenti$ =this._loadingService.showLoaderUntilCompleted(obsPagamenti$);
+
+    obsPagamenti$.subscribe(val => 
       {
         this.matDataSource.data = val;
         //this.matDataSource.paginator = this.paginator;
         //this.matDataSource.sort = this.sort;
       }
     );
+
+
   }
 
   applyFilter(event: Event) {
