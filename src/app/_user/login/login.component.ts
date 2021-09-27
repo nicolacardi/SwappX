f26851ec3fcfaba5b98e32c1c5a896556aabae9f
nicolacardi <nicola.cardi@gmail.com>
2021-09-27 09:10:13 +0200

    ...

--- src/app/_user/login/login.component.ts
+++ src/app/_user/login/login.component.ts
@@ -6,6 +6,9 @@ import {MatSnackBar, MatSnackBarConfig} from '@angular/material/snack-bar';
 //import { SidebarComponent } from '../../shared/sidebar/sidebar.component'
 
 import { UserService } from '../user.service';
+import { DialogOkComponent } from 'src/app/_components/utilities/dialog-ok/dialog-ok.component';
+import { MatDialog } from '@angular/material/dialog';
+import { SnackbarComponent } from 'src/app/_components/utilities/snackbar/snackbar.component';
 
 @Component({
   selector: 'app-login',
@@ -25,7 +28,9 @@ export class LoginComponent implements OnInit {
   constructor(private uService:       UserService, 
               private router:         Router,
               private snackBar :      MatSnackBar,
-              private fb:             FormBuilder, 
+              private fb:             FormBuilder,
+              public _dialog:         MatDialog,
+              private _snackBar:      MatSnackBar,
               ) {
     this.form = this.fb.group({
       UserName:                   ['user', Validators.required],
@@ -35,7 +40,6 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     if(localStorage.getItem('token') != null){
-
       this.router.navigateByUrl('/default');
     }
   }
@@ -48,8 +52,10 @@ export class LoginComponent implements OnInit {
       (res: any) => {
 
         this.uService.changeLoggedIn(true);
-        this.ShowMessage("Login Corretta", "Benvenuto " + this.uService.currUser.fullname, false);
-
+        //this.ShowMessage("Login Corretta", "Benvenuto " + this.uService.currUser.fullname, false);
+        this._snackBar.openFromComponent(SnackbarComponent, {
+          data: 'Benvenuto!', panelClass: ['green-snackbar']
+        });
         //Forse fa schifo ma funziona
         //this.sidebar.ngOnInit();
 
@@ -60,36 +66,50 @@ export class LoginComponent implements OnInit {
 
         this.uService.changeLoggedIn(false);
         if(err.status== 400) {
-          this.ShowMessage("Utente o Password errati", "Riprova",true);
+          //this.ShowMessage("Utente o Password errati", "Riprova",true);
+          this._snackBar.openFromComponent(SnackbarComponent, {
+            data: 'Utente o Password errati! - Riprova!', panelClass: ['green-snackbar']
+          });
         }
         else {
           console.log(err);
-          this.ShowMessage("Server Error: ", "Time-out" ,true);
+          //this.ShowMessage("Server Error: ", "Time-out" ,true);
           //this.ShowMessage(err,"" , true);
+          this._snackBar.openFromComponent(SnackbarComponent, {
+            data: 'Server Error: timeout!', panelClass: ['red-snackbar']
+          });
+          
         }
       }
     );
   }
 
 
-  ShowMessage(msg: string, title?: string, hasErrors: boolean= false ) {
-    let config = new MatSnackBarConfig();
-    config.verticalPosition  = 'bottom';
-    config.horizontalPosition = 'center';
-    config.duration = 2000;
-
-    if(hasErrors){
-      config.panelClass =  ['error-class'];
-      console.log("ShowMessage: hasErrors");
-    }
-    //else
-    //  config.panelClass =  ['ng-deep'];
-
-    if(title != null)
-      this.snackBar.open(msg, title, config);
-    else
-      this.snackBar.open(msg,"", config);
+  problemiLogin(){
+    this._dialog.open(DialogOkComponent, {
+      width: '320px',
+      data: {titolo: "HAI DIMENTICATO LA PASSWORD?", sottoTitolo: "Bravo mona!"}
+    });
   }
+
+  // ShowMessage(msg: string, title?: string, hasErrors: boolean= false ) {
+  //   let config = new MatSnackBarConfig();
+  //   config.verticalPosition  = 'bottom';
+  //   config.horizontalPosition = 'center';
+  //   config.duration = 2000;
+
+  //   if(hasErrors){
+  //     config.panelClass =  ['error-class'];
+  //     console.log("ShowMessage: hasErrors");
+  //   }
+  //   //else
+  //   //  config.panelClass =  ['ng-deep'];
+
+  //   if(title != null)
+  //     this.snackBar.open(msg, title, config);
+  //   else
+  //     this.snackBar.open(msg,"", config);
+  // }
 
 }
 
--- src/app/_user/register/register.component.ts
+++ src/app/_user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form! :                     FormGroup;
+  
+  constructor(              private fb:             FormBuilder, ) {
+    this.form = this.fb.group({
+      UserName:                   ['user', Validators.required],
+      Password:                   ['pass', { validators:[ Validators.required, Validators.maxLength(50)]}],
+      PasswordRepeat:             ['pass', { validators:[ Validators.required, Validators.maxLength(50)]}],
+      fullname:                   ['Nicola cardi', { validators:[ Validators.required, Validators.maxLength(50)]}],
+      email:                      ['nicola.cardi@gmail.com', { validators:[ Validators.required, Validators.maxLength(50)]}]
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+
   }
 
 }
