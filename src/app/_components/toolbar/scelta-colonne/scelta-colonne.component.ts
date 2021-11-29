90c8e723d9e5c09c6f9fe31914f408dc15aefcba
Andrea Svegliado <andrea.svegliado@gmail.com>
2021-11-29 15:26:14 +0100

    scelta colonne, solo qualche nota

--- src/app/_components/toolbar/scelta-colonne/scelta-colonne.component.ts
+++ src/app/_components/toolbar/scelta-colonne/scelta-colonne.component.ts
@@ -10,15 +10,40 @@ import { AlunniListComponent } from '../../alunni/alunni-list/alunni-list.compon
 })
 export class SceltaColonneComponent implements OnInit {
 
+  gridName!: string;
+  //userID!: 
+
   basket!: string[];
   items!: string[];
   
+  //1- Modificare costruttore: in input deve accettare il parametro [gridName] (l'utente va recuperato da this.uService.currentUser)
+  //ORCAPALETTA: come faccio a injettarlo ? private svcUser:        UserService,
+ 
+  /*
+  2-  Chiamare WS GET _UT_Layout [TODO asp.net core]
+  input:
+  //userID
+  //gridName
+  output:
+  //List< columnName, isVisible >
+  //
+  */
+
+  //3- Popolare le due liste in funzione del parametro isVisible
+
+  /* 
+  4- Save: Chiamare WS DELETE  (tutte le righe con chiave GridName e UserID)
+             Chiamare WS POST per tutti gli elementi delle due liste 
+  */
+
+  //5- Refresh chiamante
+
   constructor(@Inject(MAT_DIALOG_DATA) public displayedColumns: string[]) { }
 
   ngOnInit(): void {
     console.log (this.displayedColumns);
+
     this.basket = this.displayedColumns;
-    //this.basket = ['patate', 'angurie'];
     this.items = this.displayedColumns;
     this.items = ['cf', 'comuneNascita'];
     
---
+++ src/app/_models/_UT_GridLayout.ts
@@ -0,0 +1,8 @@
+export interface _UT_GridLayout {
+        id:             number;
+        userID:         string;
+        gridName:       string;
+        columnName:     string;
+        isVisible:      boolean;
+}
+
