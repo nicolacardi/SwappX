76f2312744a3b2e0bad519758b583fdb99bbddbb
nicolacardi <nicola.cardi@gmail.com>
2021-12-16 16:34:50 +0100

    ...

--- src/app/_components/alunni/alunni-list/alunni-list.component.ts
+++ src/app/_components/alunni/alunni-list/alunni-list.component.ts
@@ -221,6 +221,8 @@ export class AlunniListComponent implements OnInit {
       loadAlunni$.subscribe(val => 
         {
           this.matDataSource.data = val;
+          console.log("alunni: ", this.matDataSource.data);
+
           this.matDataSource.paginator = this.paginator;
           this.matDataSource.sort = this.sort; 
         }
