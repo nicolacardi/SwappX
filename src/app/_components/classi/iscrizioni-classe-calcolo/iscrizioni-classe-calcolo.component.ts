1888c3125a2592d914c1723d326adffa19c5972f
nicolacardi <nicola.cardi@gmail.com>
2022-02-13 18:07:18 +0100

    ...

--- src/app/_components/classi/iscrizioni-classe-calcolo/iscrizioni-classe-calcolo.component.ts
+++ src/app/_components/classi/iscrizioni-classe-calcolo/iscrizioni-classe-calcolo.component.ts
@@ -2,8 +2,8 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { iif, Observable, of, timer } from 'rxjs';
-import { concatMap, filter, finalize, map, tap } from 'rxjs/operators';
+import { iif, Observable, of } from 'rxjs';
+import { concatMap, tap } from 'rxjs/operators';
 import { ASC_AnnoScolastico } from 'src/app/_models/ASC_AnnoScolastico';
 import { CLS_ClasseSezioneAnno, CLS_ClasseSezioneAnnoGroup } from 'src/app/_models/CLS_ClasseSezioneAnno';
 import { CLS_Iscrizione } from 'src/app/_models/CLS_Iscrizione';
@@ -78,8 +78,6 @@ export class IscrizioniClasseCalcoloComponent implements OnInit {
           }),
         concatMap( ()  =>
             this.svcClasseSezioneAnno.listByAnnoGroupByClasse(this.annoSucc.id)),
-      //       )
-      // .pipe (
         tap(()=>{
           this.form.controls['selectClasseSezioneAnno'].setValue(this.classeSezioneAnno.classeSezioneAnnoSuccID);
         }  
@@ -95,84 +93,46 @@ export class IscrizioniClasseCalcoloComponent implements OnInit {
     });
     dialogYesNo.afterClosed().subscribe(async result => {
 
+    //https://advancedweb.hu/how-to-use-async-functions-with-array-foreach-in-javascript/
 
+    //Metodo suggerito per eseguire le chiamate in maniera SERIALE
+    //serve trasformare in questo modo la foreach +
+    //trasformare la subscribe nella elaboraIscrizione in una Promise() +
+    //attenderla con await async sia nel ciclo for sia nella elaboraIscrizione
 
-      //this.data.arrAlunniChecked.forEach( element => this.elaboraIscrizione(element))
-      //https://advancedweb.hu/how-to-use-async-functions-with-array-foreach-in-javascript/   BOH!
-      //https://gist.github.com/joeytwiddle/37d2085425c049629b80956d3c618971
-
-      
-
-
-    //1.Metodo suggerito per eseguire le chiamate in maniera SERIALE: non funziona
-    // for (const element of this.data.arrAlunniChecked) {
-    //   await this.elaboraIscrizione(element);
-    // }
-
-    //2.Metodo suggerito per eseguire le chiamate in maniera PARALLELA: non funziona
-    // await Promise.all(this.data.arrAlunniChecked.map(async (element) => {
-    //   await this.elaboraIscrizione(element);
-    // }));
-
-    //3.Altro metodo suggerito per eseguire le chiamate in maniera SERIALE: non funziona
-
-    await this.data.arrAlunniChecked.reduce(async (a, element) => {
-      // Wait for the previous item to finish processing
-      await a;
-      // Process this item
+    for (const element of this.data.arrAlunniChecked) {
       await this.elaboraIscrizione(element);
-    }, Promise.resolve());
+    }
 
-      
-
-
-      // let waitforthis = new Promise ((res, rej) => {
-      //   this.data.arrAlunniChecked.forEach(element => this.elaboraIscrizione(element))
-      // });
-      
-      // waitforthis.then (
-      //   () => 
-      //     {
-      //       console.log("finalmente ho finito");
-      //       this._snackBar.openFromComponent(SnackbarComponent, {data: 'Iscrizione eseguita per '+this.nRecOK+' alunni su '+(this.nRecOK +this.nRecKO), panelClass: ['green-snackbar']});
-      //     }
-      // );
-      
-      console.log ("passo di qua");
-      this._snackBar.openFromComponent(SnackbarComponent, {data: 'Iscrizione eseguita per '+this.nRecOK+' alunni su '+(this.nRecOK +this.nRecKO), panelClass: ['green-snackbar']});
+    this._snackBar.openFromComponent(SnackbarComponent, {data: 'Iscrizione eseguita per '+this.nRecOK+' alunni su '+(this.nRecOK +this.nRecKO), panelClass: ['green-snackbar']});
     })
   }
 
 
+
   async elaboraIscrizione (element: CLS_Iscrizione){
-      console.log ("step1", element.alunno);
         let objIscrizione = {
           AlunnoID: element.alunnoID,
           ClasseSezioneAnnoID: this.form.controls['selectClasseSezioneAnno'].value
         };
-      console.log ("step2", element.alunno);
-         await this.svcIscrizioni.getByAlunnoAndAnno(this.annoSucc.id, element.alunnoID)
+          await this.svcIscrizioni.getByAlunnoAndAnno(this.annoSucc.id, element.alunnoID)
 
           .pipe (
-            tap(res=> {
-              if (res != null) {this.nRecKO++}
-              console.log ("step3", element.alunno);
+            tap(res=> {if (res != null) {this.nRecKO++}
             }
             ),
-            concatMap(  async res => iif (()=> res == null,
-              await this.svcIscrizioni.post(objIscrizione)
+            concatMap(  res => iif (()=> res == null,
+              this.svcIscrizioni.post(objIscrizione)
                 .pipe(
-                  tap(()=>{this.nRecOK++;
-                  console.log("step4", element.alunno);})
+                  tap(()=>{this.nRecOK++;})
                 )
-              ,
-              of()
-              )
+                ,
+                of()
+                )
             )
           )
-          .subscribe(
-            () => {console.log ("nRecKO:"+this.nRecKO, "nrecOK:"+this.nRecOK);}
-          )  }
+          .toPromise(); //necessario per ATTENDERE che finisca insieme alla await e async a inizio funzione
   }
+}
 
 
