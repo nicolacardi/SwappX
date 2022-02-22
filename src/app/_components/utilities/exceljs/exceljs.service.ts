1ac5e8ae8ee7a757a4f2499d8bae3fde4054d58c
nicolacardi <nicola.cardi@gmail.com>
2022-02-22 20:05:30 +0100

    service xlsx

--- src/app/_components/utilities/exceljs/exceljs.service.ts
+++ src/app/_components/utilities/exceljs/exceljs.service.ts
@@ -5,7 +5,6 @@ import * as fs from 'file-saver';
 import { DatePipe } from '@angular/common';
 import { Cell } from 'jspdf-autotable';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -16,7 +15,6 @@ export class ExcelService {
     
 
 //#region PREPARAZIONE DEI DATI ***************************************************************************
-
     //creo l'array per l'operazione di flattening
     let flattened : any= [];
 
@@ -37,11 +35,11 @@ export class ExcelService {
     //Ora trasformo un array di objects in un array di arrays
     let data = subsetFromFlattened.map((obj: { [s: string]: unknown; } | ArrayLike<unknown>) => Object.values(obj)); 
 //#endregion FINE PREPARAZONE DEI DATI ********************************************************************
-
     
     //Create workbook and worksheet
     let workbook = new Workbook();
     let worksheet = workbook.addWorksheet('Report');
+
     worksheet.headerFooter.oddFooter = "Pag. &P di &N";
 
     //Add Row and formatting
@@ -49,14 +47,6 @@ export class ExcelService {
     titleRow.font = { name: 'Helvetica', family: 4, size: 16, underline: 'double', bold: true }
     worksheet.addRow([]);
     worksheet.addRow(['Date : ' + this.datePipe.transform(new Date(), 'medium')])
-    //Add Image
-    // let logo = workbook.addImage({
-    //   base64: logoFile.logoBase64,
-    //   extension: 'png',
-    // });
-    //worksheet.addImage(logo, 'E1:F3');
-
-    //worksheet.mergeCells('A1:D2');
     
     //Blank Row 
     worksheet.addRow([]);
@@ -71,15 +61,25 @@ export class ExcelService {
         fgColor: { argb: 'FF2273a3' }, //colore dello sfondo (fgColor??)
         bgColor: { argb: 'FFFFFFFF' } //una cella ha sia un fgcolor che un bgcolor entrambi di riempimento a quanto pare
       }
-
       cell.border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } }
     })
-    // worksheet.addRows(data);
-    // Add Data and Conditional Formatting
-    data.forEach((d: any) => {
-      let row = worksheet.addRow(d);
-      // let qty = row.getCell(5);
+    
+    //costruisco un array di widths che contiene tanti elementi quanti data[0].length.
+    let widthsArr = [0]; //parto da un array che in prima posizione ha uno 0...
+    console.log ("data[0]", data[0]);
+    fieldsToKeep.forEach(element => 
+      widthsArr.push(element.length)//per ogni elemento del primo elemento di data aggiungo uno 0.
+    )
 
+    data.forEach((d: any) => {
+
+      d.forEach((element: any, index: number) =>{
+        let lenText = element ? element.length : 0;
+        if (lenText > widthsArr[index + 1 ])  widthsArr[index + 1] = lenText; 
+      })
+
+      let row = worksheet.addRow(d); //scrivo i dati
+      // let qty = row.getCell(5);
       // let color = 'FF99FF99';
       //       // if (qty.value < 500) {
       //       //   color = 'FF9999'
@@ -89,55 +89,23 @@ export class ExcelService {
       //   pattern: 'solid',
       //   fgColor: { argb: color }
       // }
+      
+    });
+
+    //widthsArr contiene la lunghezza dei testi della tabella
+    for (let i = 1; i < widthsArr.length; i++) {
+      //in excel la larghezza impotata in pixels NON E' PROPORZIONALE a quella in punti!
+      //la relazione è wpunti = wpixels/6  -5/6 (infatti la lunghezza in pixel nonè mai minore di 5)
+      //se uso ncaratteri per impostare la larghezza, questa sarà più stretta per numeri bassi e più alta per numeri alti...non va bene
+      //infatti lui moltiplica i caratteri per 6 e ottiene la misura in pixels, ma questa si traduce poi in lunghezze non proporzionali
+      //voglio invece impostare la larghezza in maniera proporzionale al numero di caratteri
+      worksheet.getColumn(i).width = (widthsArr[i]*6+5)/6;
     }
-    );
-    worksheet.getColumn(3).width = 30;
-    worksheet.getColumn(4).width = 30;
-    worksheet.addRow([]);
-    //Footer Row
-    // let footerRow = worksheet.addRow(['This is system generated excel sheet.']);
-    // footerRow.getCell(1).fill = {
-    //   type: 'pattern',
-    //   pattern: 'solid',
-    //   fgColor: { argb: 'FFCCFFE5' }
-    // };
-    // footerRow.getCell(1).border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } }
-    //Merge Cells
-    // worksheet.mergeCells(`A${footerRow.number}:F${footerRow.number}`);
-
-
-
-
-    // worksheet.columns.forEach(function(column){
-    //   var dataMax = 0;
-    //   column.eachCell!({ includeEmpty: true }, function(cell){
-    //     var columnLength = cell.value?.toString().length || 0;	//si ferma qui
-    //     if (columnLength > dataMax) {
-    //       dataMax = columnLength;
-    //      }
-    //        })
-    //        column.width = dataMax < 5 ? 5 : dataMax;
-    // });
-
-    // let dataMax: number[];
-    // let max: number;
-    // columns.forEach((column: Column) => {
-    //     dataMax = [];
-    //     column.eachCell({includeEmpty: false}, (cell: Cell) => {
-    //         dataMax.push(cell.value?.toString().length || 0);
-    //     });
-    //     max = Math.max(...dataMax);
-    //     column.width = max < 10 ? 10 : max;
-    // });
-
 
     //Generate Excel File with given name
     workbook.xlsx.writeBuffer().then((data) => {
       let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-
       const d = new Date();
-
-
       fs.saveAs(blob, d.toISOString().split('T')[0]+"_"+fileName);
     })
   }
