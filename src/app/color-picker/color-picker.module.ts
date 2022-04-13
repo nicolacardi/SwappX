42b842f71ccdb65ed427978a440d47b2366439ef
nicolacardi <nicola.cardi@gmail.com>
2022-04-13 10:06:50 +0200

    color1

---
+++ src/app/color-picker/color-picker.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ColorPickerComponent } from '../_components/color-picker/color-picker.component';
+
+
+@NgModule({
+  declarations: [
+    ColorPickerComponent,
+  ],
+  imports: [
+    CommonModule
+    
+
+  ]
+})
+export class ColorPickerModule { }
