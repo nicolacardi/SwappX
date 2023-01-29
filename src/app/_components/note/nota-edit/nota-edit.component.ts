f87e2a9b6f0bd9a0e95fece3f7b302edf6be980f
nicolacardi <nicola.cardi@gmail.com>
2023-01-29 11:16:18 +0100

    ...

--- src/app/_components/note/nota-edit/nota-edit.component.ts
+++ src/app/_components/note/nota-edit/nota-edit.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup }               from '@angular/forms';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar }                          from '@angular/material/snack-bar';
 import { Observable }                           from 'rxjs';
-import { concatMap, tap }                                  from 'rxjs/operators';
+import { concatMap, finalize, tap }                                  from 'rxjs/operators';
 
 //components
 import { DialogYesNoComponent }                 from '../../utilities/dialog-yes-no/dialog-yes-no.component';
@@ -23,6 +23,8 @@ import { ScadenzePersoneService }               from '../../scadenze/scadenze-pe
 import { DOC_Nota }                             from 'src/app/_models/DOC_Nota';
 import { DialogDataNota }                       from 'src/app/_models/DialogData';
 import { CAL_Scadenza, CAL_ScadenzaPersone }    from 'src/app/_models/CAL_Scadenza';
+import { NoteIscrizioniService } from '../noteiscrizioni.service';
+import { DOC_NotaIscrizione } from 'src/app/_models/DOC_NotaIscrizione';
 
 
 @Component({
@@ -50,7 +52,10 @@ export class NotaEditComponent implements OnInit {
   constructor( 
     public _dialogRef:                          MatDialogRef<NotaEditComponent>,
     private svcUser:                            UserService,
+    private svcNote:                            NoteService,
     private svcIscrizioni:                      IscrizioniService,
+    private svcNoteIscrizioni:                  NoteIscrizioniService,
+
     private svcScadenze:                        ScadenzeService,
     private svcScadenzePersone:                 ScadenzePersoneService,
 
@@ -58,7 +63,6 @@ export class NotaEditComponent implements OnInit {
 
     @Inject(MAT_DIALOG_DATA) public data:       DialogDataNota,
     private fb:                                 FormBuilder, 
-    private svcNote:                            NoteService,
     private _loadingService:                    LoadingService,
     public _dialog:                             MatDialog,
     private _snackBar:                          MatSnackBar ) {
@@ -69,7 +73,7 @@ export class NotaEditComponent implements OnInit {
     {
       id:                                       [null],
       iscrizioneID:                             [],
-      iscrizioneIDMultiple:                     [],
+      iscrizioni:                               [],
       personaID:                                [],
       alunnoID:                                 [],
       dtNota:                                   [],
@@ -79,9 +83,10 @@ export class NotaEditComponent implements OnInit {
       ckFirmato:                                [],
       dtFirma:                                  [],
       ckInvioMsg:                               [false],
-      scadenzaID:                               [],    
       nota:                                     []
     });
+
+    this.obsIscrizioni$ = this.svcIscrizioni.listByClasseSezioneAnno(this.data.classeSezioneAnnoID);
   }
 
   ngOnInit() {
@@ -92,9 +97,8 @@ export class NotaEditComponent implements OnInit {
 
     if (!this.data.notaID || this.data.notaID + '' == "0") {
       console.log ("nota-edit loadData nota, nuova nota");
-      this.obsIscrizioni$ = this.svcIscrizioni.listByClasseSezioneAnno(this.data.classeSezioneAnnoID);
 
-      this.form.controls.iscrizioneID.setValue(this.data.iscrizioneID);
+      this.form.controls.iscrizioni.setValue(this.data.iscrizioni);
       this.form.controls.dtNota.setValue(new Date());
 
       this.svcUser.obscurrentUser.subscribe( val => {
@@ -111,20 +115,19 @@ export class NotaEditComponent implements OnInit {
     } 
     else {
 
-      this.obsIscrizioni$ = this.svcIscrizioni.listByClasseSezioneAnno(this.data.classeSezioneAnnoID);
-
+      let iscrizioniArr : number[] = [];
       //in teoria potrei anche fare a meno dell'observable e dell'async nel form: potrei passare TUTTI i valori tramite DialogData...tanto li ho già...ma non sarebbe rxJs
       const obsNota$: Observable<DOC_Nota> = this.svcNote.get(this.data.notaID);
       const loadNota$ = this._loadingService.showLoaderUntilCompleted(obsNota$);
-      
       this.obsNota$ = loadNota$
       .pipe( tap(
         nota => {
           //console.log ("nota-edit loadData nota", nota);
           this.form.patchValue(nota);
-          this.iscrizioneID= this.data.iscrizioneID;
-          this.alunnoNomeCognome = nota.iscrizione?.alunno.persona.nome! + ' '+ nota.iscrizione?.alunno.persona.cognome!;
-          this.form.controls.alunnoID.setValue(nota.iscrizione?.alunnoID);
+          for (let i=0; i < nota._NotaIscrizioni.length ; i++){ 
+            iscrizioniArr.push(nota._NotaIscrizioni[i].iscrizioneID)
+          }
+          this.form.controls.iscrizioni.setValue(iscrizioniArr);
           this.personaID = this.data.personaID;
           this.personaNomeCognome = nota.persona?.nome! + " " +nota.persona?.cognome!;
         } )
@@ -143,28 +146,76 @@ export class NotaEditComponent implements OnInit {
       result => {
         if(result){
 
-          let scadenzaIDTodelete = this.form.controls.scadenzaID.value;
 
-          
-          this.svcNote.delete (this.data.notaID).subscribe(
-            res =>{
-              //vanno prima cancellati i record di scadenzapersone!
-              if (scadenzaIDTodelete){
-                this.svcScadenzePersone.deleteByScadenza(scadenzaIDTodelete)
-                .pipe(
-                  concatMap(()=> this.svcScadenze.delete(scadenzaIDTodelete))
-                )
-                .subscribe(
-                  res=>{console.log ("cancellazione scadenza e persone a buon fine");},
-                  err=>{console.log ("errore in cancellazione scadenza o cancellazione persone")}
-                )
-              }
+          //bisogna prima cancellare eventuali scadenze con notaID dentro e conseguentemente scadenzePersone con quelle scadenzeID dentro
+          //nell'ordine:
+            //1. cancellare da ScadenzePersone
+            //2. cancellare da Scadenze
+            //3. cancellare da NoteIscrizioni
+            //4. cancellare da Note
 
+
+
+            // this.svcScadenze.listByNota(this.data.notaID)
+            // subscribe(
+            //   //ciclo for e non forEach!!
+            //   (res: CAL_Scadenza) => res.forEach(
+            //     this.svcScadenzePersone.deleteByScadenza(res.id)
+            //     this.svcScadenze.delete(res.id)
+            //     .subscribe(
+            //       //E SE NON CE NE SONO DI SCADENZE?
+            //     )
+            //   )
+            //   //ora posso cancellare le scadenze
+            // )
+
+
+            
+            // this.svcScadenzePersone.deleteByScadenza(scadenzaIDTodelete)
+            //       .pipe(
+            //         concatMap(()=> this.svcScadenze.deleteByNota(this.data.notaID))
+            //       )
+            //       .subscribe(
+            //         res=>{console.log ("cancellazione scadenza e persone a buon fine");},
+            //         err=>{console.log ("errore in cancellazione scadenza o cancellazione persone")}
+            //       )
+
+
+          this.svcNoteIscrizioni.deleteByNota(this.data.notaID)
+            .pipe(
+              concatMap(()=> this.svcNote.delete(this.data.notaID))
+            )
+          .subscribe(
+            res=> {
               this._snackBar.openFromComponent(SnackbarComponent,{data: 'Record cancellato', panelClass: ['red-snackbar']});
               this._dialogRef.close();
             },
-            err => this._snackBar.openFromComponent(SnackbarComponent, {data: 'Errore in cancellazione', panelClass: ['red-snackbar']})
+            err => this._snackBar.openFromComponent(SnackbarComponent, {data: 'Errore in cancellazione', panelClass: ['red-snackbar']}) 
           );
+
+
+          // let scadenzaIDTodelete = this.form.controls.scadenzaID.value;
+
+          
+          // this.svcNote.delete (this.data.notaID).subscribe(
+          //   res =>{
+          //     //vanno prima cancellati i record di scadenzapersone!
+          //     if (scadenzaIDTodelete){
+          //       this.svcScadenzePersone.deleteByScadenza(scadenzaIDTodelete)
+          //       .pipe(
+          //         concatMap(()=> this.svcScadenze.delete(scadenzaIDTodelete))
+          //       )
+          //       .subscribe(
+          //         res=>{console.log ("cancellazione scadenza e persone a buon fine");},
+          //         err=>{console.log ("errore in cancellazione scadenza o cancellazione persone")}
+          //       )
+          //     }
+
+          //     this._snackBar.openFromComponent(SnackbarComponent,{data: 'Record cancellato', panelClass: ['red-snackbar']});
+          //     this._dialogRef.close();
+          //   },
+          //   err => this._snackBar.openFromComponent(SnackbarComponent, {data: 'Errore in cancellazione', panelClass: ['red-snackbar']})
+          // );
         }
       }
     );
@@ -218,22 +269,22 @@ export class NotaEditComponent implements OnInit {
       h_Ini: this.form.controls.h_Ini.value,
       h_End: this.form.controls.h_End.value,
       PersonaID: this.personaID,
-      TipoScadenzaID: 6  //Fa schifetto
+      TipoScadenzaID: 6,  //Fa schifetto  TODO
     }
 
     //console.log ("nota-edit - save this.form:", this.form.value);
 
     //ci possono essere 6 casi
-    //1 this.form.controls.scadenzaID == null && this.form.controls.id.value == null && this.form.controls.ckInvioMsg.value == true
-    //2 this.form.controls.scadenzaID == null && this.form.controls.id.value == null && this.form.controls.ckInvioMsg.value == false
+    //00 this.form.controls.scadenzaID == null && this.form.controls.id.value == null && this.form.controls.ckInvioMsg.value == false
+
     //3 this.form.controls.scadenzaID == null && this.form.controls.id.value != null && this.form.controls.ckInvioMsg.value == true
     //4 this.form.controls.scadenzaID == null && this.form.controls.id.value != null && this.form.controls.ckInvioMsg.value == false
     //5 this.form.controls.scadenzaID != null && this.form.controls.id.value != null && this.form.controls.ckInvioMsg.value == true
     //6 this.form.controls.scadenzaID != null && this.form.controls.id.value != null && this.form.controls.ckInvioMsg.value == false
 
     //(non è possibile che se scadenzaID != null id sia != null perchè scadenzaID è null se id della nota c'è)
-    //caso 1 faccio la POST della scadenza e con scadenzaID faccio la POST della nota
-    //caso 2 faccio solo la POST della nota, scadenzaID resta null
+        //caso 00 faccio solo la POST della nota, scadenzaID resta null
+
     //caso 3 faccio la POST della scadenza e con scadenzaID faccio la PUT della nota    
     //caso 4 scadenzaID resta null, faccio la PUT della nota
     //caso 5 faccio la PUT della scadenza e faccio la PUT della nota, anche asincrone
@@ -244,55 +295,71 @@ export class NotaEditComponent implements OnInit {
     // console.log ("nota-edit - this.form.controls.ckInvioMsg.value:", this.form.controls.ckInvioMsg.value);
 
 
-    //caso 1 : Nuova Nota + ScadenzaID non esiste ancora (ovviamente) + è stato selezionato l'invio del Messaggio
-    if (this.form.controls.scadenzaID.value == null && this.form.controls.id.value == null && this.form.controls.ckInvioMsg.value == true) {
-      console.log ("caso1");
-      //POST SCADENZA
-      objScadenza.id = 0;
-      //this.svcLezioni.post(this.form.value).subscribe(
-      this.svcScadenze.post(objScadenza).subscribe(
-        res => {
-          
-          this.form.controls.scadenzaID.setValue(res.id);
-          //POST NOTA usando ScadenzaID appena creata (callback hell?)
-          this.form.controls['iscrizioneIDMultiple'].value.forEach((iscrizioneID: number) =>{
-            this.form.controls.iscrizioneID.setValue(iscrizioneID);
-            //console.log("vado a inserire i genitori dell'alunno che è iscritto con", iscrizioneID);
-            //a partire da ciascuna iscrizioneID estraggo l'alunnoID e ne inserisco in ScadenzaPersone i genitori
-            this.svcIscrizioni.get(iscrizioneID).subscribe(
-              iscrizione => this.insertGenitori(iscrizione.alunnoID, res.id)
-            )            
-            
-            this.svcNote.post(this.form.value).subscribe(
-              res=> {
-                this._dialogRef.close();
-                this._snackBar.openFromComponent(SnackbarComponent, {data: 'Record salvato', panelClass: ['green-snackbar']});
-              },
-              err=> this._snackBar.openFromComponent(SnackbarComponent, {data: 'Errore in salvataggio', panelClass: ['red-snackbar']})
-            );
-          });
-          console.log ("inserimento Scadenza a buon fine - ScadenzaID:", res.id);
-        },
-        err =>  console.log ("errore inserimento Scadenza")
-      );
-    }
-    //OK caso 2 : Nuova Nota + ScadenzaID non esiste ancora (ovviamente) + NON è stato selezionato l'invio del Messaggio
-    if (this.form.controls.scadenzaID.value == null && this.form.controls.id.value == null && this.form.controls.ckInvioMsg.value == false) {
-      console.log ("caso2");
-      //POST SCADENZA
-      objScadenza.id = 0;
-      this.form.controls['iscrizioneIDMultiple'].value.forEach((iscrizioneID: number) =>{
-        this.form.controls.iscrizioneID.setValue(iscrizioneID);
-        this.svcNote.post(this.form.value).subscribe(
-          res=> {
+
+    if (this.form.controls.id.value == null) {
+      // caso 00 : Nuova Nota
+      console.log ("nuova nota");     
+      this.svcNote.post(this.form.value)
+      .subscribe(
+        nota=> {
+            this.form.controls.iscrizioni.value.forEach( (iscrizione: number) => {
+            const objNotaIscrizione = <DOC_NotaIscrizione>{
+              notaID: nota.id,
+              iscrizioneID: iscrizione
+            } 
+            this.svcNoteIscrizioni.post(objNotaIscrizione).subscribe();
             this._dialogRef.close();
             this._snackBar.openFromComponent(SnackbarComponent, {data: 'Record salvato', panelClass: ['green-snackbar']});
-          },
-          err=> this._snackBar.openFromComponent(SnackbarComponent, {data: 'Errore in salvataggio', panelClass: ['red-snackbar']})
-        );
-      });
+          })
+
+          if(this.form.controls.ckInvioMsg.value) {
+            console.log ("invioMsg");
+            //DEVO INSERIRE TANTE SCADENZE QUANTE LE ISCRIZIONI
+            //serve notaID appena inserito
+            this.form.controls.iscrizioni.value.forEach( (iscrizione: number) => {
+              this.svcIscrizioni.get(iscrizione)
+              .subscribe(
+                (iscrizione:CLS_Iscrizione) => {
+                  //console.log ("iscrizione estratta:", iscrizione)
+                  //inserisco la scadenza
+                  const objScadenzaX = <CAL_Scadenza>{
+                    dtCalendario: this.form.controls.dtNota.value,
+                    title: "NOTA DISCIPLINARE PER " + iscrizione.alunno!.persona.nome + ' ' + iscrizione.alunno!.persona.cognome +"-" + this.form.controls.nota.value,
+                    start: this.form.controls.dtNota.value,
+                    end: this.form.controls.dtNota.value,
+                    color: "#FF0000",
+                    ckPromemoria: true,
+                    ckRisposta: false,
+                    h_Ini: this.form.controls.h_Ini.value,
+                    h_End: this.form.controls.h_End.value,
+                    PersonaID: this.personaID,
+                    TipoScadenzaID: 6,  //Fa schifetto  TODO
+                    NotaID: nota.id
+                  }
+                  //console.log ("objScadenzaX", objScadenzaX);
+                  this.svcScadenze.post(objScadenzaX)
+                  .subscribe(
+                    scad => {
+                      //inserisco i genitori nella tabella ScadenzaPersone
+                      this.insertGenitori(iscrizione.alunnoID, scad.id)
+                    }
+                  )
+                }
+              ) 
+            })
+          }
+        },
+        err=>this._snackBar.openFromComponent(SnackbarComponent, {data: 'Errore in salvataggio', panelClass: ['red-snackbar']})
+      )
+    } else {
+      
+
     }
-    //OK caso 3 : Nota Esistente + ScadenzaID non c'è in precedenza + è stato selezionato l'invio del Messaggio
+
+
+
+
+    // caso 3 : Nota Esistente + ScadenzaID non c'è in precedenza + è stato selezionato l'invio del Messaggio
     if (this.form.controls.scadenzaID.value == null && this.form.controls.id.value != null && this.form.controls.ckInvioMsg.value == true) {
       console.log ("caso3");
       //POST SCADENZA
@@ -316,12 +383,28 @@ export class NotaEditComponent implements OnInit {
         err =>  console.log ("errore inserimento Scadenza")
       );
     }
-    //OK caso 4 : Nota Esistente + ScadenzaID non c'è in precedenza + NON è stato selezionato l'invio del Messaggio
+
+    // caso 4 : Nota Esistente + ScadenzaID non c'è in precedenza + NON è stato selezionato l'invio del Messaggio
     if (this.form.controls.scadenzaID.value == null && this.form.controls.id.value != null && this.form.controls.ckInvioMsg.value == false) {
       console.log ("caso4");
       //PUT NOTA
-      this.svcNote.put(this.form.value).subscribe(
+      this.svcNote.put(this.form.value)
+      .pipe(
+        concatMap( val=> this.svcNoteIscrizioni.deleteByNota(this.data.notaID))
+      )
+      .subscribe(
         res=> {
+          //nel concatMap ho PULITO tutto, ora vado a ripostare le iscrizioni presenti nella combo multiple
+          this.form.controls.iscrizioni.value.forEach( (iscrizione: number) => {
+            
+            const objNotaIscrizione = <DOC_NotaIscrizione>{
+              notaID: this.data.notaID,
+              iscrizioneID: iscrizione
+            }
+            
+            this.svcNoteIscrizioni.post(objNotaIscrizione).subscribe();
+          }
+          )
           this._dialogRef.close();
           this._snackBar.openFromComponent(SnackbarComponent, {data: 'Record salvato', panelClass: ['green-snackbar']});
         },
@@ -330,7 +413,7 @@ export class NotaEditComponent implements OnInit {
     }
 
 
-    //OK caso 5 : Nota Esistente + ScadenzaID c'è già + è stato selezionato l'invio del Messaggio
+    // caso 5 : Nota Esistente + ScadenzaID c'è già + è stato selezionato l'invio del Messaggio
     if (this.form.controls.scadenzaID.value != null && this.form.controls.id.value != null && this.form.controls.ckInvioMsg.value == true) {
       console.log ("caso5");
       //PUT SCADENZA
@@ -383,7 +466,7 @@ export class NotaEditComponent implements OnInit {
 
   insertGenitori(alunnoID: number, scadenzaID: number) {
 
-    //va sempre inserito il maestro
+    //Se si volesse inserire il maestro tra coloro che lo ricevono...
     let objScadenzaPersona: CAL_ScadenzaPersone = {
       personaID: this.form.controls.personaID.value,
       scadenzaID : scadenzaID,
