dab509db16ed15534efc1bebac16b60054921bfd
nicolacardi <nicola.cardi@gmail.com>
2023-08-18 13:10:37 +0200

    ....

--- src/app/_components/procedura-iscrizione/procedura-iscrizione.component.ts
+++ src/app/_components/procedura-iscrizione/procedura-iscrizione.component.ts
@@ -19,6 +19,8 @@ import { IscrizioniService }                    from '../iscrizioni/iscrizioni.s
 import { CLS_Iscrizione }                       from 'src/app/_models/CLS_Iscrizione';
 import { ALU_Genitore }                         from 'src/app/_models/ALU_Genitore';
 import { ALU_GenitoreAlunno }                   from 'src/app/_models/ALU_GenitoreAlunno';
+import { ConsensiIscrizioneComponent } from './consensi-iscrizione/consensi-iscrizione.component';
+import { dA } from '@fullcalendar/core/internal-common';
 
 //#endregion
 @Component({
@@ -42,6 +44,8 @@ export class ProceduraIscrizioneComponent implements OnInit {
 //#region ----- ViewChild Input Output ---------
 
   @ViewChildren(PersonaFormComponent) PersonaFormComponent!: QueryList<PersonaFormComponent>;
+  @ViewChild('formConsensiIscrizione') ConsensiFormComponent!: ConsensiIscrizioneComponent;
+
   @ViewChild('stepper') stepper!:               MatStepper;
 //#endregion
 
@@ -116,6 +120,73 @@ export class ProceduraIscrizioneComponent implements OnInit {
       next: res=> this._snackBar.openFromComponent(SnackbarComponent, {data: 'Record salvato', panelClass: ['green-snackbar']}),
       error: err=> this._snackBar.openFromComponent(SnackbarComponent, {data: 'Errore in salvataggio', panelClass: ['red-snackbar']})
     });
+
+  }
+
+  salvaConsensi() {
+    let formValues = this.ConsensiFormComponent.formConsensi.value;
+    console.log (formValues);
+    //devo trasformare questo ogetto in un altro
+    //ad esempio da
+    // const formValues = {
+    //   3: true,
+    //   8: '3',
+    //   11: true,
+    //   13: true,
+    //   14: 2,
+    //   15: true,
+    //   16: 3,
+    //   17: 4,
+    //   18: 5
+    // }; 
+    //deve diventare
+
+    // consensoID risposta1 risposta2 risposta3 risposta4 risposta5
+    // 3 true false false false false
+    // 8 false false true false false
+    // 11 true false false false false
+    // 13 true false false false false
+    // 14 false true false false false
+    // 15 true false false false false
+    // 16 false false true false false
+
+    const transformedData = [];
+
+    for (const key in formValues) {
+      if (formValues.hasOwnProperty(key)) {
+        const value = formValues[key];
+        const consensoId = parseInt(key);
+        const risposta1 = value === true || parseInt(value) === 1 ? true : false;
+        const risposta2 = parseInt(value) === 2 ? true : false;
+        const risposta3 = parseInt(value) === 3 ? true : false;
+        const risposta4 = parseInt(value) === 4 ? true : false;
+        const risposta5 = parseInt(value) === 5 ? true : false;
+    
+        const transformedEntry = {
+          ConsensoID: consensoId,
+          Risposta1: risposta1,
+          Risposta2: risposta2,
+          Risposta3: risposta3,
+          Risposta4: risposta4,
+          Risposta5: risposta5,
+        };
+    
+        transformedData.push(transformedEntry);
+      }
+    }
+    console.log (transformedData);
+
+    //vanno aggiunti gli altri campi di CLS_ConsensoIscrizione, tra cui iscrizioneID
+
+    
+    //ora devo salvarlo, va fatto un service consensiiscrizione
+    //cancello quel che c'è prima
+    //await firstValueFrom(this.svcConsensiIscrizione.deleteByIscrizione(this.iscrizioneID));
+    // this.svcConsensiIscrizione.post(transformedData).subscribe({
+    //   next: res=> this._snackBar.openFromComponent(SnackbarComponent, {data: 'Consensi salvati', panelClass: ['green-snackbar']}),
+    //   error: err=> this._snackBar.openFromComponent(SnackbarComponent, {data: 'Errore in salvataggio consensi', panelClass: ['red-snackbar']})
+    // });
+
   }
 
   formValidEmitted(valid: boolean){
